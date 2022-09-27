9a1874a64988fe2427c50231230e454911afe366
Bhone15 <bhonepyaeaung15@gmail.com>
2022-09-27 19:12:11 +0630

    first store category account app

---
+++ store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import store, product_detail
+
+urlpatterns = [
+  path('', store, name='store'),
+  path('<slug:category_slug>/', store, name='products_by_category' ),
+  path('<slug:category_slug>/<slug:product_slug>/', product_detail, name='product_detail' )
+]
