5d449fb4c410581ed56f9e5b3e0dd0a05feb4b2d
Bhone15 <bhonepyaeaung15@gmail.com>
2022-09-29 11:26:28 +0630

    done carts function by using session key

--- carts/urls.py
+++ carts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import cart
+from .views import add_cart, remove_cart, cart, remove_cart_item
 
 urlpatterns = [
-  path('', cart, name='cart')
+  path('', cart, name='cart'),
+  path('add_cart/<int:product_id>/', add_cart, name='add_cart'),
+  path('remove_cart/<int:product_id>/', remove_cart, name='remove_cart'),
+  path('remove_cart_item/<int:product_id>/', remove_cart_item, name='remove_cart_item')
 ]
